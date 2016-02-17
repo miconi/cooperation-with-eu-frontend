b2dd586403fed214609bf0df437feb266bfdfad9
Michał Oniszczuk <michal.oniszczuk@gmail.com>
2016-02-17 13:27:31 +0100

    Now threshold can be changed through a form in UI.

--- app/main.js
+++ app/main.js
@@ -1,15 +1,26 @@
-requirejs(["require", "linqjs", "jquery", "datamaps", "DataProvider"], function(require, Enumerable, $, Datamap) {
+requirejs(["require", "linqjs", "jquery", "datamaps", "DataProvider"], function (require, Enumerable, $, Datamap) {
     var map = new Datamap({
         element: document.getElementById("basic"),
         projection: 'mercator'
     });
     var dataProvider = new DataProvider("data/connections.csv");
+    changeThreshold();
 
-    dataProvider.getConnectionsWithPositions(makeMyMap, 20000);
+    $("#threshold-button").click(changeThreshold);
+    $("#threshold-input").keyup(function (e) {
+        if (e.keyCode === 13) {
+            changeThreshold();
+        }
+    });
 
-    function makeMyMap(error, connections) {
+    function changeThreshold() {
+        var threshold = $("#threshold-input").val();
+        dataProvider.getConnectionsWithPositions(updateMap, threshold);
+    }
+
+    function updateMap(error, connections) {
         console.log(connections);
-        map.arc(connections,  {strokeWidth: 1, arcSharpness: 1.4});
+        map.arc(connections, {strokeWidth: 1, arcSharpness: 1.4});
     }
 
 });
