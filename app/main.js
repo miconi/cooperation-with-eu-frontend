dbf2a166fd1c76a71e671816ebf7d1285dd09b9c
Michał Oniszczuk <michal.oniszczuk@gmail.com>
2016-02-26 20:01:11 +0100

    Zoomin & dragging.

--- app/main.js
+++ app/main.js
@@ -36,9 +36,38 @@ requirejs(["require", "linqjs", "jquery", "datamaps", "DataProvider"], function 
             .on("mouseover", function (d) {
                 console.log(d);
                 $('#connectionInfo').text(d.origin.country + " — " + d.destination.country + ": " + d.value + " co-authored papers");
-            })
-        ;
-
+            });
     }
 
+
+    // Zooming & dragging bahaviour, based on
+    // https://bl.ocks.org/mbostock/6123708
+    var zoom = d3.behavior.zoom()
+        .scaleExtent([1, 10])
+        .on("zoom", zoomed);
+
+    var drag = d3.behavior.drag()
+        .origin(function(d) { return d; })
+        .on("dragstart", dragstarted)
+        .on("drag", dragged)
+        .on("dragend", dragended);
+
+    d3.selectAll("svg").call(zoom);
+
+    function zoomed() {
+        d3.selectAll("svg g").attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    }
+
+    function dragstarted(d) {
+        d3.event.sourceEvent.stopPropagation();
+        d3.select(this).classed("dragging", true);
+    }
+
+    function dragged(d) {
+        d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+    }
+
+    function dragended(d) {
+        d3.select(this).classed("dragging", false);
+    }
 });
